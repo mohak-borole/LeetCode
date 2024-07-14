2347a9d09d2036c5a02516e674a9f2a8298a25d2
mohak borole <98516851+mohak-borole@users.noreply.github.com>
2024-07-14 23:17:34 +0530

    Time: 0 ms (100.00%), Space: 8 MB (6.85%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        
+        for(int i = 31 ; i >= 0; i--) {
+            if((n&(1<<i)) != 0) count++;
+        }
+        
+        return count;
+    }
+};
